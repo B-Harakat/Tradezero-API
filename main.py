3750143e17c51a458fc88fbad1c9bbcd7b6f958a
M.B.X <77303923+Harakat-Bjorn@users.noreply.github.com>
2022-04-06 11:40:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from Tradezero_API import Tradezero_API
 
 zero = Tradezero_API()
 
-zero.login("XXXXX","xxxxxxx")
+zero.login(usr_name="XXXXX",pass_word="xxxxxxx")
 
 # Give time to load page
+# Adjust according to your internet speed 
 time.sleep(25)
 print("Ready for order")
 
+
 zero.submit_order(ticker = "DNN", quantity = 1, order_type = "Stop-LMT",price = 8.4, sprice = 7, action = "Sell")
-print("submitted")
-time.sleep(10)
+
+# Above execution takes some time, hence this buffer before next order
+time.sleep(1)
+
 zero.submit_order(ticker = "DNN", quantity = 1, order_type = "Stop-LMT",price = 8.4, sprice = 7.3, action = "Sell")
-print("DIDIT")
+
+print("Done!")
 
 
 
